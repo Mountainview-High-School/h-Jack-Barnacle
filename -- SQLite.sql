03827269a94487919a7a2eb4c56dce98f994faaf
Jack-Barnacle <23barclayj@mountainview.school.nz>
2026-03-05 09:54:27 +1300

    Database

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -12,3 +12,18 @@ select * from Student WHERE first_name = 'Gene';
 select * from Student WHERE last_name like '%y';
 
 -- 4
+select * from Class WHERE NOT room_id = 44;
+
+-- 5
+select * from Class WHERE room_id in (43, 44);
+
+-- 6
+select * from Class WHERE room_id in (43);
+
+-- 7
+select teacher_name, room_id from class 
+inner join Teacher
+on class.teacher_id = Teacher.teacher_id
+WHERE room_id in (43, 44);
+
+-- 8
