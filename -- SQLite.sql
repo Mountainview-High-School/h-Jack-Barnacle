bc0a916521bb530df30c039e679494453a86bb2c
Jack-Barnacle <23barclayj@mountainview.school.nz>
2026-03-09 12:17:33 +1300

    i did the 9 quetion

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -27,3 +27,14 @@ on class.teacher_id = Teacher.teacher_id
 WHERE room_id in (43, 44);
 
 -- 8
+select Student.student_id, first_name, last_name, room_id from Student
+inner join StudentClass on Student.student_id = StudentClass.student_id
+inner join Class on StudentClass.class_id = Class.class_id
+WHERE room_id in (44);
+
+-- 9
+select Student.student_id, first_name, last_name, room_id, Teacher.teacher_name from Student
+inner join StudentClass on Student.student_id = StudentClass.student_id
+inner join Class on StudentClass.class_id = Class.class_id
+inner join Teacher on Class.teacher_id = Teacher.teacher_id
+WHERE Teacher.teacher_id in (3);
